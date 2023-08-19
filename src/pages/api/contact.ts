63821743373949ecabc3e13467e442df8cd3de17
ayoub boudra <ayoub.boudra1@gmail.com>
2023-08-19 14:15:28 +0100

    nodemailer work

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -2,22 +2,18 @@
 import { EmailInput } from '@/components/Contact'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer')
 
 const email = process.env.EMAIL
-const pass  = process.env.EMAIL_PASS
+const pass = process.env.EMAIL_PASS
 
 export const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: email,
-    pass: pass
-  }
-});
-
-
-
+    pass: pass,
+  },
+})
 
 const handler = (
   req: NextApiRequest,
@@ -26,30 +22,23 @@ const handler = (
   if (req.method === 'POST') {
     const data: EmailInput = req.body
 
-    
-
     const mailOptions = {
       from: email,
       to: email,
       subject: `from ${data.emailAdresse} by the name of ${data.firstName} ${data.lastName}`,
-      text: data.message
-    };
-
-    
+      text: data.message,
+    }
 
     if (data.emailAdresse || data.firstName || data.lastName || data.message) {
-
-
-
-        transporter.sendMail(mailOptions, function(error:any, info:any){
-          if (error) {
-              console.log(error.message)
-              return res.status(400).json({
-                message : error.message
-              })
-          }
-        });
-        return res.status(200).json({message : 'Send Success'})
+      transporter.sendMail(mailOptions, function (error: any, info: any) {
+        if (error) {
+          console.log(error.message)
+          return res.status(400).json({
+            message: error.message,
+          })
+        }
+      })
+      return res.status(200).json({ message: 'Send Success' })
     }
   }
 
@@ -57,6 +46,5 @@ const handler = (
     message: 'Bad request',
   })
 }
-
 
 export default handler
