b2c606a80f5b4770cf7cc6eafb27ab0acc09c154
ayoub boudra <ayoub.boudra1@gmail.com>
2023-08-21 19:36:41 +0100

    fixing modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -13,10 +13,6 @@ function Modal({ showModal, setShowModal }: ModalProps) {
       <div>
         <div>
           <div className="flex fixed inset-0 z-[99] w-screen h-screen bg-white">
-            {/* <button onClick={()=>setShowModal(false)}  className="absolute top-0 right-0 z-30 flex items-center justify-center px-3 py-2 mt-3 mr-3 space-x-1 text-xs font-medium uppercase border rounded-md border-neutral-200 lg:border-white/20 lg:bg-black/10 hover:lg:bg-black/30 text-neutral-600 lg:text-white hover:bg-neutral-100">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-4 h-4"><path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" /></svg>
-                      <span>Close</span>
-                    </button> */}
             <button
               type="button"
               className="absolute top-0 right-0 z-30 flex items-center justify-center px-2 py-2 mt-5 mr-5 space-x-1 text-xs font-medium uppercase border rounded-md border-neutral-200 lg:border-white/20 lg:bg-black/10 hover:lg:bg-black/30 text-neutral-600 lg:text-white hover:bg-neutral-100"
@@ -39,10 +35,10 @@ function Modal({ showModal, setShowModal }: ModalProps) {
               </svg>
               <span className="sr-only">Close modal</span>
             </button>
-            <div className="relative flex flex-wrap mt-12 w-full h-full p-2">
+            <div className="relative flex flex-wrap  mt-12 w-full h-full md:h-1/2 p-2">
               <div className="flex relative flex-col lg:flex-row ">
                 <div className="flex justify-center  relative">
-                  <div className="mx-6 ">
+                  <div className="mx-6 flex items-center">
                     <Image
                       width={1535}
                       height={733}
