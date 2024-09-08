67f92c1f53e1f955250b2a80ca68474c7f17219b
ayoubboudra1 <ayoub.boudra1@gmail.com>
2024-09-08 21:30:18 +0100

    change skills color

--- src/components/OneSkill.tsx
+++ src/components/OneSkill.tsx
@@ -10,10 +10,10 @@ export interface OneSkillProps {
 function OneSkill({ skillTitle, skillsName }: OneSkillProps) {
   return (
     <>
-      <h3 className="text-2xl font-bold ">{skillTitle}</h3>
+      <h3 className="text-2xl font-bold text-gray-900 ">{skillTitle}</h3>
       <div className="list-disc list-outside my-4">
         {skillsName.map((skill, index) => (
-          <i key={'tag'+index} className="bg-gray-100 text-gray-800 text-lg font-semibold mx-2  px-2.5 py-2.5 rounded-full  " style={{'display':'inline-block','margin':'5px'}}>{skill}</i>
+          <i key={'tag'+index} className="bg-yellow-400 text-white cursor-pointer hover:scale-105 text-lg font-semibold mx-2  px-2.5 py-2.5 rounded-full  " style={{'display':'inline-block','margin':'5px'}}>{skill}</i>
 
       ))}
 
